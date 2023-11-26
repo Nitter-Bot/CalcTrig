b086927aad9134c4e32f3f0c675aea0a92c3c448
Samuel <83435582+Nitter-Bot@users.noreply.github.com>
2023-11-25 23:05:00 -0600

    Add files via upload

---
+++ FuncionFactorial/Factorial.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+float factorial(int num);
+
+int main()
+{
+	int number;
+	float fact;
+	printf("Ingrese numero:\n");
+	scanf("%i",&number);
+	fact=factorial(number);
+	printf("%f",fact);
+}
+
+float factorial(int num)
+{
+	float principal=1;
+	principal=num;
+	while(num>1)
+	{
+		num--;
+		principal*=num;
+	}
+	return principal;
+}
