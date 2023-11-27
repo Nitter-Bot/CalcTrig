226c4ddeadabe7d67c5fd1f4174a49dcc24712aa
Samuel <83435582+Nitter-Bot@users.noreply.github.com>
2023-11-26 18:05:52 -0600

    Update Código.c
    El calculo de radianes lo cambie adentro de la función.

--- FuncionCoseno/Código.c
+++ FuncionCoseno/Código.c
@@ -4,7 +4,7 @@ double convertirARadianes(double grados) {
     return grados * (3.14159265358979323846 / 180.0);
 }
 
-double calcularFactorial(int n) {
+/*double calcularFactorial(int n) {
     double factorial = 1.0;
 
     for (int i = 1; i <= n; i++) {
@@ -12,17 +12,17 @@ double calcularFactorial(int n) {
     }
 
     return factorial;
-}
+}*/
 
 double calcularCoseno(double x) {
     double resultado = 1.0;
     double termino = 1.0;
     int n = 1;
-
-    while (termino > 0.0001 || termino < -0.0001) {
+    x=convertirRadianes(x)//Cambiamos el angulo a radianes
+    while (termino > 0.0000001 || termino < -0.0000001) {
         termino = (termino * x * x) / (2 * n * (2 * n - 1));
         if (n % 2 == 0) {
-            resultado += termino;
+            resultado += termino;@
         } else {
             resultado -= termino;
         }
@@ -39,11 +39,8 @@ int main() {
     printf("Ingresa el ángulo en grados: ");
     scanf("%lf", &angulo);
 
-    // Convierte el ángulo a radianes
-    double anguloRadianes = convertirARadianes(angulo);
-
     // Calcula el coseno y muestra el resultado
-    double resultado = calcularCoseno(anguloRadianes);
+    double resultado = calcularCoseno(angulo);
     printf("El coseno de %lf grados es aproximadamente: %lf\n", angulo, resultado);
 
     return 0;
