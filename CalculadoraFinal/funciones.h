2972fdb16bfaba6aa6dbabe0dfdced1a7a1c1063
Samuel <83435582+Nitter-Bot@users.noreply.github.com>
2023-11-26 20:59:42 -0600

    Update funciones.h

--- CalculadoraFinal/funciones.h
+++ CalculadoraFinal/funciones.h
@@ -5,7 +5,7 @@ double valorAbsoluto(double numero);
 double raizCuadrada(int numero);
 float factorial(int num);
 double convertirARadianes(double grados);
-
+double seno();
 double calcularCoseno(double x);
 //Potencias
 float potencia(float x,float y)
@@ -14,7 +14,7 @@ float potencia(float x,float y)
 	if(y==0){return 1;}
 	else
 	{
-		if(y>1)
+		if(y>=1)
 		{//Potencias positivas
 			do{
 				val*=x;
@@ -71,7 +71,34 @@ double convertirARadianes(double grados) {
     return grados * (3.14159265358979323846 / 180.0);
 }
 //Seno
-
+double seno()
+{
+	double sen0, sen1, n1, aux,x;
+	int n;
+	printf("Ingrese el angulo: ");//Se pide angulo
+	scanf("%lf", &x);
+	x = convertirARadianes(x);//Se convierte angulo a radianes
+	sen1 = 0;
+	sen0 = 1;
+	n = 1;
+	
+	while(sen0 > 0.0000000001 || sen0 < -0.0000000001)//while que genera la serie
+	{
+		n1 = factorial(n);
+		aux=potencia(x,n);
+		sen0 = aux/n1;
+		if(n%4 == 1)
+		{
+			sen1 += sen0;
+		}
+		else if(n%4 == 3)
+		{
+			sen1 -= sen0;
+		}
+		n += 2;
+	}
+	return sen1;	
+}
 //Coseno
 double calcularCoseno(double x) {
     double resultado = 1.0;
