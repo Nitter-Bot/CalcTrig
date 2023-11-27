ad2636d84c5c8e5e33db77fa724f16ff88bbedd1
AldoEZ <143436994+AldoEZ@users.noreply.github.com>
2023-11-26 19:55:49 -0600

    Update Seno.c

--- FuncionSeno/Seno.c
+++ FuncionSeno/Seno.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include "funciones.h"
 
+int main()
+{
+    double sen0, sen1, n1, aux, z, rad, x;
+    int n;
+    printf("Ingrese el angulo: ");//Se pide angulo
+    scanf("%lf", &x);
+    rad = convrad(x);//Se convierte angulo a radianes
+    sen1 = 0;
+    sen0 = 1;
+    n = 1;
+    while(sen0 > 0.0000000001 || sen0 < -0.0000000001)//while que genera la serie
+    {
+        n1 = factorial(n);
+        sen0 = potencia(rad, n)/n1;
+        if(n%4 == 1)
+        {
+            sen1 += sen0;
+        }
+        else if(n%4 == 3)
+        {
+            sen1 -= sen0;
+        }
+        n += 2;
+    }
+    printf("El seno es: %lf.\n", sen1);//se imprime el resultado
+
+}
